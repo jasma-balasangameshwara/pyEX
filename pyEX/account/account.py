5524d3ae17b0d7e499e15befe93675c244ff446c
Jasma Balasangameshwara <95459583+jasma-balasangameshwara@users.noreply.github.com>
2022-06-05 17:36:49 +0530

    Update account.py

--- pyEX/account/account.py
+++ pyEX/account/account.py
@@ -5,6 +5,7 @@
 # This file is part of the pyEX library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
+
 from functools import wraps
 
 import pandas as pd
